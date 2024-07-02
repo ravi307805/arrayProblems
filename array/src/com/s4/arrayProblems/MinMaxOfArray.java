2d34999795c0301573ca6e7b8b3a79fc53b4ebec
Ravi Ranjan Pandey <Ravi Ranjan Pandey@DESKTOP-9NO9E7D>
2024-07-02 23:16:23 +0530

    Finding Min and Max from an array

---
+++ array/src/com/s4/arrayProblems/MinMaxOfArray.java
@@ -0,0 +1,29 @@
+package com.s4.arrayProblems;
+
+public class MinMaxOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {  4,11, 6, 9, 18 };
+		int[] result=minMaxArray(arr);
+		System.out.println("Minimum: "+result[0]+" Maximum: "+result[1]);
+
+	}
+
+	private static int[] minMaxArray(int[] arr) {
+		// TODO Auto-generated method stub
+		int min = arr[0];
+		int max= arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i]< min) {
+				min=arr[i];
+			}
+			if (arr[i]> max) {
+				max=arr[i];
+			}
+		}
+		return new int[] {min, max};
+		
+	}
+}
+	
