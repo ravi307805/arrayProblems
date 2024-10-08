fd0c06fce38d84baa7e1ce6207d5af04d58cb681
Ravi Ranjan Pandey <Ravi Ranjan Pandey@DESKTOP-9NO9E7D>
2024-10-08 16:25:03 +0530

    2nd largest without sorting updated

--- array/src/com/s4/arrayProblems/SecondLargest.java
+++ array/src/com/s4/arrayProblems/SecondLargest.java
@@ -3,19 +3,20 @@ package com.s4.arrayProblems;
 public class SecondLargest {
 
 	public static void main(String[] args) {
-		int arr[]= {3,4,16,227,29,22,20};
+		int arr[] = { 3, 4, 16, 227, 29, 22, 20 };
 		secondLargest(arr);
 	}
-	//Second Largest Element in an Array without sorting
+
+	// Second Largest Element in an Array without sorting
 	private static void secondLargest(int[] arr) {
-		int largest=arr[0];
-		int secondLargest=arr[1];
- 		for (int i = 1; i < arr.length; i++) {
-			if (largest<arr[i]) {
-				secondLargest=largest;
-				largest=arr[i];
+		int largest = arr[0];
+		int secondLargest = arr[1];
+		for (int i = 1; i < arr.length; i++) {
+			if (largest < arr[i]) {
+				secondLargest = largest;
+				largest = arr[i];
 			} else if (arr[i] > secondLargest && arr[i] != largest) {
-				secondLargest=arr[i];
+				secondLargest = arr[i];
 			}
 		}
 		System.out.println(secondLargest);
